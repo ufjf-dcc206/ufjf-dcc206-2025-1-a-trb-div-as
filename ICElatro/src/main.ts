83bcb4554c63b4d6dc84d1d650b899b99626f7a2
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-07-27 15:53:21 -0300

    adiciona função para iniciar o jogo

--- ICElatro/src/main.ts
+++ ICElatro/src/main.ts
@@ -2,7 +2,20 @@ import "./style.css";
 
 //adiciona botão
 const botaoJogar = document.getElementById("bJogar") as HTMLButtonElement;
-botaoJogar.addEventListener("click", () => {});
-
 const botaoTutorial = document.getElementById("bTutorial") as HTMLButtonElement;
+
+//tela inicial e tela jogo
+const telaInicial = document.getElementById("telaInicial") as HTMLDivElement;
+const telaJogo = document.getElementById("telaJogo") as HTMLDivElement;
+
+//função para iniciar o jogo
+function iniciarJogo(telaInicial: HTMLDivElement, telaJogo: HTMLDivElement) {
+  telaInicial.style.display = "none"; //tira a tela inicial
+  telaJogo.style.display = "block"; //inicia a tela do jogo
+};
+
+botaoJogar.addEventListener("click", () => {
+  iniciarJogo(telaInicial, telaJogo);
+});
+
 botaoTutorial.addEventListener("click", () => {});
