ced29baf0ab9e6c25f2ebd2dbafbc148c1465614
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-07-27 15:19:52 -0300

    adiciona e centraliza título e botão

--- ICElatro/src/main.ts
+++ ICElatro/src/main.ts
@@ -1,24 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+//adiciona botão
+const botaoJogar = document.getElementById("bJogar") as HTMLButtonElement;
+botaoJogar.addEventListener("click", () => {});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const botaoTutorial = document.getElementById("bTutorial") as HTMLButtonElement;
+botaoTutorial.addEventListener("click", () => {});
