2fd4fee315e90ce6a6dc85fde834ef00c46afe77
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-07-31 10:49:41 -0300

    adiciona função para renderizar a mão e altera ícone da página

--- ICElatro/src/main.ts
+++ ICElatro/src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-//adiciona botão
+//botões
 const botaoJogar = document.getElementById("bJogar") as HTMLButtonElement;
 const botaoTutorial = document.getElementById("bTutorial") as HTMLButtonElement;
 
@@ -8,25 +8,26 @@ const botaoTutorial = document.getElementById("bTutorial") as HTMLButtonElement;
 const telaInicial = document.getElementById("telaInicial") as HTMLDivElement;
 const telaJogo = document.getElementById("telaJogo") as HTMLDivElement;
 
-//cartas
+//tipo cartas
 type Cartas = {
   nome: string | number;
   naipe: string;
   pontos: number;
 };
 
-// criando as cartas do baralho
+//criação do baralho
 const baralho: Cartas[] = [];
 ["copas", "paus", "espadas", "ouros"].forEach((n) => {
   [2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((v) => {
     baralho.push({ nome: v, naipe: n, pontos: v });
   });
-  ["J", "Q", "k"].forEach((v) => {
+  ["J", "Q", "K"].forEach((v) => {
     baralho.push({ nome: v, naipe: n, pontos: 10 });
   });
   baralho.push({ nome: "A", naipe: n, pontos: 15 });
 });
 
+//criação da mão
 let mao: Cartas[] = [];
 
 //função para embaralhar o baralho
@@ -51,6 +52,51 @@ function maoInicial(baralho: Cartas[]) {
   }
 }
 
+//função para renderizar a mão na tela
+function renderizarMão() {
+  const cartasNaMao = document.getElementById("cartasNaMao") as HTMLDivElement; //"cartasNaMao" = a mão completa
+  mao.forEach((carta) => {
+    const cartaUnica = document.createElement("div"); //"cartaUnica" = carta individual
+    cartaUnica.classList.add("carta");
+
+    //nome da carta
+    const nomeCarta = document.createElement("span");
+    nomeCarta.classList.add("nomeCarta");
+    nomeCarta.textContent = String(carta.nome);
+
+    //símbolo do naipe da carta
+    const iconeNaipe = document.createElement("span");
+    iconeNaipe.classList.add("iconeNaipe");
+    let simboloNaipe = "";
+    switch (carta.naipe) {
+      case "copas":
+        simboloNaipe = "♥";
+        cartaUnica.classList.add("naipeVermelho");
+        break;
+      case "ouros":
+        simboloNaipe = "♦";
+        cartaUnica.classList.add("naipeVermelho");
+        break;
+      case "espadas":
+        simboloNaipe = "♠";
+        cartaUnica.classList.add("naipePreto");
+        break;
+      case "paus":
+        simboloNaipe = "♣";
+        cartaUnica.classList.add("naipePreto");
+        break;
+    }
+    iconeNaipe.textContent = simboloNaipe;
+
+    //adiciona "nomeCarta" e "iconeNaipe" na "cartaUnica"
+    cartaUnica.appendChild(nomeCarta);
+    cartaUnica.appendChild(iconeNaipe);
+
+    //adiciona "cartaUnica" no "cartasNaMao"
+    cartasNaMao.appendChild(cartaUnica);
+  });
+}
+
 //função para iniciar o jogo
 function iniciarJogo(
   telaInicial: HTMLDivElement,
@@ -60,9 +106,10 @@ function iniciarJogo(
   telaInicial.style.display = "none"; //tira a tela inicial
   telaJogo.style.display = "block"; //inicia a tela do jogo
   maoInicial(baralho);
-  console.log(mao);
+  renderizarMão();
 }
 
+//ações dos botões "Jogar" e "Tutorial"
 botaoJogar.addEventListener("click", () => {
   iniciarJogo(telaInicial, telaJogo, baralho);
 });
