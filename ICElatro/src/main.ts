050ad89dcde87454b784e407226392a364f24ccb
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2025-08-03 21:22:34 -0300

    adiciona função ao botão "jogar" e  cria um array para as cartas jogadas

--- ICElatro/src/main.ts
+++ ICElatro/src/main.ts
@@ -131,6 +131,50 @@ botaoJogar.addEventListener("click", () => {
 botaoTutorial.addEventListener("click", () => {});
 
 //ações dos botões "Jogar" e "Descartar" (dentro do jogo)
-botaoJogarCartas.addEventListener("click", () => {});
+botaoJogarCartas.addEventListener("click", () => {
+  const cartasSelecionadas = document.querySelectorAll(".carta.selecionada");
+  if (cartasSelecionadas.length === 0) {
+      console.log("Nenhuma carta foi selecionada");
+      return;
+  }
+  const cartasJogadas: Cartas[] = [];
+
+  cartasSelecionadas.forEach((elementoCarta) => {
+
+    const nomeCartaElemento = elementoCarta.querySelector(".nomeCarta");
+    const iconeNaipeElemento = elementoCarta.querySelector(".iconeNaipe");
+
+    if (!nomeCartaElemento || !iconeNaipeElemento) {
+      console.error("Não foi possível encontrar o nome ou naipe da carta.");
+      return;
+    }
+
+    const nomeCarta = nomeCartaElemento.textContent;
+    const iconeNaipe = iconeNaipeElemento.textContent;
+
+    let naipe = "";
+    switch (iconeNaipe) {
+      case "♥":
+        naipe = "copas";
+        break;
+      case "♦":
+        naipe = "ouros";
+        break;
+      case "♠":
+        naipe = "espadas";
+        break;
+      case "♣":
+        naipe = "paus";
+        break;
+    }
+    const cartaEncontrada = mao.find((c) => String(c.nome) === nomeCarta && c.naipe === naipe);
+    if (cartaEncontrada) {
+      cartasJogadas.push(cartaEncontrada);
+    }
+
+  });
+  console.log("Cartas jogadas:", cartasJogadas);
+  
+});
 
 botaoDescartarCartas.addEventListener("click", () => {});
